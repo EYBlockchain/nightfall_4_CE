eca2438caf4c30ceec0da4711ce48254df0d841d
westlad <duncan.westland@uk.ey.com>
2025-07-01 09:19:25 +0100

    feat: mock deployer deployed by anvil container

--- anvil/entrypoint.sh
+++ anvil/entrypoint.sh
@@ -1,11 +1,26 @@
-  forge_command(&["install"]);
-    forge_command(&[
-        "script",
-        "MockDeployer",
-        "--fork-url",
-        &settings.ethereum_client_url,
-        "--broadcast",
-        "--force",
-    ]);
+#!/bin/sh
+set -e
 
-    exec "$@"
+# Start anvil in the background
+anvil --base-fee 58000000000 --block-time 5 &
+ANVIL_PID=$!
+
+# Wait for anvil to be ready (JSON-RPC POST)
+for i in $(seq 1 30); do
+    if curl -s -X POST --data '{"jsonrpc":"2.0","id":1,"method":"eth_chainId","params":[]}' \
+        -H "Content-Type: application/json" http://localhost:8545 | grep -q '"result"'; then
+        break
+    fi
+    echo "Waiting for anvil to be ready... ($i)"
+    sleep 1
+done
+
+# Run the deployment script with Foundry
+forge script MockDeployer \
+    --fork-url ws://localhost:8545 \
+    --broadcast \
+    --force
+
+wait $ANVIL_PID
+
+exec "$@"
