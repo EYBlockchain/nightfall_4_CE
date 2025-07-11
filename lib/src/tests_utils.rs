98a3ec2df3d1b10f7090f17f2a08c4587cf379e4
kkahina <kahina.khacef@uk.ey.com>
2025-07-11 08:55:07 +0100

    update testcontainers version 0.24.0

--- lib/src/tests_utils.rs
+++ lib/src/tests_utils.rs
@@ -14,13 +14,12 @@ pub fn get_db_connection_uri(host: Host, port: u16) -> String {
 }
 
 pub async fn get_mongo() -> ContainerAsync<GenericImage> {
-    GenericImage::new("mongo", "4.4.1-bionic")
+    let mongo_image = GenericImage::new("mongo", "4.4.1-bionic")
         .with_exposed_port(27017.tcp())
         .with_wait_for(WaitFor::message_on_stdout("Waiting for connections"))
-        .with_startup_timeout(Duration::from_secs(120))
-        .start()
-        .await
-        .unwrap()
+        .with_startup_timeout(Duration::from_secs(120));
+
+    mongo_image.start().await.unwrap()
 }
 
 pub async fn get_db_connection(container: &ContainerAsync<GenericImage>) -> mongodb::Client {
