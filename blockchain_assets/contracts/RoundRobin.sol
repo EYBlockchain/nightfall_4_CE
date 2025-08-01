ff9118d39ab46aa2acc34398cf33db970645fb98
Swati Rawal <swati.rawal@uk.ey.com>
2025-08-01 12:25:57 +0100

    fix test

--- blockchain_assets/contracts/RoundRobin.sol
+++ blockchain_assets/contracts/RoundRobin.sol
@@ -108,7 +108,7 @@ contract RoundRobin is ProposerManager, Certified {
 
     function rotate_proposer() external override {
         require(can_rotate(), "It is not time to rotate the proposer");
-        if (nightfall.layer2_block_number() == start_l2_block)
+        if (nightfall.layer2_block_number() == start_l2_block + int(FINALIZATION_BLOCKS))
             ding_proposer(current.addr);
         current = proposers[current.next_addr];
         start_l1_block = block.number;
