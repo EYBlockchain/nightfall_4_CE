68ca77ea8bcecbe624ce1951e1303df8376529b1
Swati Rawal <swati.rawal@uk.ey.com>
2025-08-04 10:10:03 +0100

    fix clippy

--- nightfall_bindings/src/artifacts.rs
+++ nightfall_bindings/src/artifacts.rs
@@ -1,18 +1,15 @@
 use alloy::sol;
-
 sol!(
     #[sol(rpc)]
     #[derive(Debug)]
     Nightfall,
     "../blockchain_assets/artifacts/Nightfall.sol/Nightfall.json"
 );
-
 sol!(
     #[sol(rpc)]
     X509,
     "../blockchain_assets/artifacts/X509.sol/X509.json"
 );
-
 sol!(
     #[sol(rpc)]
     #[derive(Debug)]
@@ -28,18 +25,21 @@ sol!(
     IERC1155,
     "../blockchain_assets/artifacts/IERC1155.sol/IERC1155.json"
 );
+
 sol!(
     #[sol(rpc)]
     #[derive(Debug)]
     IERC20,
     "../blockchain_assets/artifacts/IERC20.sol/IERC20.json"
 );
+
 sol!(
     #[sol(rpc)]
     #[derive(Debug)]
     IERC3525,
     "../blockchain_assets/artifacts/IERC3525.sol/IERC3525.json"
 );
+
 sol!(
     #[sol(rpc)]
     #[derive(Debug)]
@@ -47,24 +47,28 @@ sol!(
     "../blockchain_assets/artifacts/IERC721.sol/IERC721.json"
 );
 
+#[cfg(feature = "artifacts_exist")]
 sol!(
     #[sol(rpc)]
     #[derive(Debug)]
     ERC1155Mock,
     "../blockchain_assets/artifacts/ERC1155Mock.sol/ERC1155Mock.json"
 );
+#[cfg(feature = "artifacts_exist")]
 sol!(
     #[sol(rpc)]
     #[derive(Debug)]
     ERC20Mock,
     "../blockchain_assets/artifacts/ERC20Mock.sol/ERC20Mock.json"
 );
+#[cfg(feature = "artifacts_exist")]
 sol!(
     #[sol(rpc)]
     #[derive(Debug)]
     ERC3525Mock,
     "../blockchain_assets/artifacts/ERC3525Mock.sol/ERC3525Mock.json"
 );
+#[cfg(feature = "artifacts_exist")]
 sol!(
     #[sol(rpc)]
     #[derive(Debug)]
