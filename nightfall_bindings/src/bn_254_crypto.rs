dbe1cb93fda18750b9be0d94ef316830c4d96a42
kkahina <kahina.khacef@uk.ey.com>
2025-09-01 11:32:27 +0100

    delete rollup-verifier

--- nightfall_bindings/src/bn_254_crypto.rs
+++ nightfall_bindings/src/bn_254_crypto.rs
@@ -26,13 +26,13 @@ pub mod bn_254_crypto {
         __abi,
     );
     #[rustfmt::skip]
-    const __BYTECODE: &[u8] = b"`U`2`\x0B\x82\x82\x829\x80Q_\x1A`s\x14`&WcNH{q`\xE0\x1B_R_`\x04R`$_\xFD[0_R`s\x81S\x82\x81\xF3\xFEs\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\x000\x14`\x80`@R_\x80\xFD\xFE\xA2dipfsX\"\x12 \xA5\xF5\xF8d@\xF6\x0F\xA1\xBC\x8E\xB7\xBA\x16\xD1\xBE5\x05Z\x07%\xD0*\xA3\x1Fz\x01\xE2\x12\xFD\xF8|\x93dsolcC\0\x08\x18\x003";
+    const __BYTECODE: &[u8] = b"`U`2`\x0B\x82\x82\x829\x80Q_\x1A`s\x14`&WcNH{q`\xE0\x1B_R_`\x04R`$_\xFD[0_R`s\x81S\x82\x81\xF3\xFEs\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\x000\x14`\x80`@R__\xFD\xFE\xA2dipfsX\"\x12 M\xEE\xE3\x0C\xD7\x96\xE3 \xB6 n\x0B\xBE\xC8P\xC3:\xC1f\x0CV\xE5\xA8\xAB\x974r\xA1\xBB\x15\xC6*dsolcC\0\x08\x1C\x003";
     /// The bytecode of the contract.
     pub static BN254CRYPTO_BYTECODE: ::ethers::core::types::Bytes = ::ethers::core::types::Bytes::from_static(
         __BYTECODE,
     );
     #[rustfmt::skip]
-    const __DEPLOYED_BYTECODE: &[u8] = b"s\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\x000\x14`\x80`@R_\x80\xFD\xFE\xA2dipfsX\"\x12 \xA5\xF5\xF8d@\xF6\x0F\xA1\xBC\x8E\xB7\xBA\x16\xD1\xBE5\x05Z\x07%\xD0*\xA3\x1Fz\x01\xE2\x12\xFD\xF8|\x93dsolcC\0\x08\x18\x003";
+    const __DEPLOYED_BYTECODE: &[u8] = b"s\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\x000\x14`\x80`@R__\xFD\xFE\xA2dipfsX\"\x12 M\xEE\xE3\x0C\xD7\x96\xE3 \xB6 n\x0B\xBE\xC8P\xC3:\xC1f\x0CV\xE5\xA8\xAB\x974r\xA1\xBB\x15\xC6*dsolcC\0\x08\x1C\x003";
     /// The deployed bytecode of the contract.
     pub static BN254CRYPTO_DEPLOYED_BYTECODE: ::ethers::core::types::Bytes = ::ethers::core::types::Bytes::from_static(
         __DEPLOYED_BYTECODE,
