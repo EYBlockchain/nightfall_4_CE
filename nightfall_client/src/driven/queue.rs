f5de99d5989dc417aa94ca6c97920c2b4c58d4f0
jiajieey <jiajie.zhang@uk.ey.com>
2025-09-12 14:40:29 +0100

    add info about withdraw response

--- nightfall_client/src/driven/queue.rs
+++ nightfall_client/src/driven/queue.rs
@@ -98,6 +98,7 @@ where
                         warn!("No webhook URL provided, skipping notification of successful transaction");
                     } else {
                         // Publish the notification
+                        info!("Response: {:?}", response);
                         publisher.publish(response).await;
                     }
                 }
