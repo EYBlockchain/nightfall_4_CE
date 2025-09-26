d2a74651114ea94684b33a4e49c797048ca64ce8
jiajieey <jiajie.zhang@uk.ey.com>
2025-09-26 06:38:48 +0100

    fix(client): add event listener manager

--- nightfall_proposer/src/main.rs
+++ nightfall_proposer/src/main.rs
@@ -8,13 +8,12 @@ use nightfall_client::driven::{
 use nightfall_proposer::{
     driven::{mock_prover::MockProver, rollup_prover::RollupProver},
     drivers::{
-        blockchain::{
-            block_assembly::start_block_assembly, nightfall_event_listener::start_event_listener,
-        },
+        blockchain::block_assembly::start_block_assembly,
         rest::routes,
     },
 };
 use std::error::Error;
+use nightfall_proposer::drivers::blockchain::event_listener_manager::ensure_running;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
@@ -41,22 +40,17 @@ async fn main() -> Result<(), Box<dyn Error>> {
         info!("Using RollupProver");
         tokio::spawn(start_block_assembly::<P, RollupProver, N>())
     };
-    let settings = get_settings();
-    let max_event_listener_attempts_proposer = settings
-        .nightfall_proposer
-        .max_event_listener_attempts
-        .unwrap_or(10);
+
     // start the event listener
-    let task_1 = tokio::spawn(start_event_listener::<P, E, N>(
-        settings.genesis_block,
-        max_event_listener_attempts_proposer,
-    ));
+    // ── start the (owned) event listener once ─────────────────────────────────
+    ensure_running::<P, E, N>().await;
+
     let routes = routes::<P, E>();
     let task_2 = tokio::spawn(warp::serve(routes).run(([0, 0, 0, 0], 3000)));
     info!("Starting warp server, block assembler and event_handler threads");
     // we'll run the warp server and blockchain listener in parallel in separate threads
     // this maybe overkill so look at combining them into a single thread - depending on speed.
-    let (_r0, _r1, _r2) = (task_0.await??, task_1.await?, task_2.await?);
+    let (_r0, _r2) = (task_0.await??, task_2.await?);
     error!("Proposer exited unexpectedly. See information above.");
     Ok(())
 }
