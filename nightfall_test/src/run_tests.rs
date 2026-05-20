74593fcff9436a2a024250db7133548a5302c827
Swati Rawal <swati.rawal@uk.ey.com>
2026-05-20 12:40:48 +0100

    chore: added some new tests  due new master changes for swaps

--- nightfall_test/src/run_tests.rs
+++ nightfall_test/src/run_tests.rs
@@ -1387,10 +1387,14 @@ pub async fn run_tests(
     let client2_erc20_after =
         get_erc20_balance(&http_client, Url::parse("http://client2:3000").unwrap()).await;
 
-    assert_eq!(client1_erc3525_slot7_after, client1_erc3525_slot7_before);
-    assert_eq!(client1_erc3525_slot8_after, client1_erc3525_slot8_before);
-    assert_eq!(client2_erc3525_slot7_after, client2_erc3525_slot7_before);
-    assert_eq!(client2_erc3525_slot8_after, client2_erc3525_slot8_before);
+    assert_eq!(
+        client1_erc3525_slot7_after + client1_erc3525_slot8_after,
+        client1_erc3525_slot7_before + client1_erc3525_slot8_before
+    );
+    assert_eq!(
+        client2_erc3525_slot7_after + client2_erc3525_slot8_after,
+        client2_erc3525_slot7_before + client2_erc3525_slot8_before
+    );
     assert_eq!(client1_erc20_after, client1_erc20_before);
     assert_eq!(client2_erc20_after, client2_erc20_before);
 
